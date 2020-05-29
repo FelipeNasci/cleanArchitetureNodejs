11c8d3b4336febe7d0a511cd745229b51693be46
felipeNasci <dfnascimento@live.com>
2020-05-29 10:48:38 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,18 +1,19 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
 
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (httpRequest.body.name === undefined) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new MissingParamError('name')
       }
     }
 
     if (httpRequest.body.email === undefined) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: email')
+        body: new MissingParamError('email')
       }
     }
   }
